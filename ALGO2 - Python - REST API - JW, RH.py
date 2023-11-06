eb2ae3241bc71628d091cdd047649ca4fc71b4a5
jwin15 <124175086+jwin15@users.noreply.github.com>
2023-11-06 13:45:41 -0500

    ALGO2 - Python - REST API - JW, RH.py

--- ALGO2 - Python - REST API - JW, RH.py
+++ ALGO2 - Python - REST API - JW, RH.py
@@ -1,47 +1,25 @@
-# This is a python example algorithm using REST API for the RIT ALGO2 Case
-
 import signal
 import requests
 from time import sleep
 
-
-# this class definition allows us to print error messages and stop the program when needed
+# Define a class for custom exceptions
 class ApiException(Exception):
     pass
 
-# this signal handler allows for a graceful shutdown when CTRL+C is pressed
+# Define the signal handler function
 def signal_handler(signum, frame):
     global shutdown
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     shutdown = True
 
 # set your API key to authenticate to the RIT client
-API_KEY = {'X-API-Key': 'YOUR API KEY HERE'}
+API_KEY = {'X-API-Key': '1J7HXV8J'}
 shutdown = False
 # other settings for market making algo
-SPREAD = 0.02
-BUY_VOLUME = 500
-SELL_VOLUME = 1000
-
-# delete orders older than 5 ticks
-num_orders = len(orders)
-while num_orders > 4:
-    orderid = orders[num_orders-1]['order_id']
-    s.delete('http://localhost:9999/v1/orders/{}'.format(orderid))
-    sleep(0.2)
-    orders = get_orders(s, 'OPEN')
-    num_orders = len(orders)
-
-# delete orders when number of orders exceeds 4
-num_orders = len(orders)
-while num_orders > 4:
-    orderid = orders[num_orders-1]['order_id']
-    s.delete('http://localhost:9999/v1/orders/{}'.format(orderid))
-    sleep(0.2)
-    orders = get_orders(s, 'OPEN')
-    num_orders = len(orders)
-
-sleep(1)
+SPREAD = 0.015
+BUY_VOLUME = 2500
+SELL_VOLUME = 2500
+orders = []  # Initialize the 'orders' list
 
 # this helper method returns the current 'tick' of the running case
 def get_tick(session):
@@ -70,16 +48,16 @@ def buy_sell(session, to_buy, to_sell, last):
     session.post('http://localhost:9999/v1/orders', params=buy_payload)
     session.post('http://localhost:9999/v1/orders', params=sell_payload)
 
-# this helper method gets all the orders of a given type (OPEN/TRANSACTED/CANCELLED)
+    # this helper method gets all the orders of a given type (OPEN/TRANSACTED/CANCELLED)
 def get_orders(session, status):
     payload = {'status': status}
-    resp = session.get('http://localhost:9999/v1/orders', params=payload)
+    resp = session.get('http://localhost:9999/v1/orders',params=payload)
     if resp.status_code == 401:
-        raise ApiException('The API key provided in this Python code must match that in the RIT client (please refer to the API hyperlink in the client toolbar and/or the RIT – User Guide – REST API Documentation.pdf)')
+        raise ApiException('The API key provided in this Python code must match that in the RIT client (please refer to the API hyperlink in the client toolbar and/or the RIT REST API Documentation.pdf)')
     orders = resp.json()
     return orders
 
-# this is the main method containing the actual market making strategy logic
+# Define the main function
 def main():
     # creates a session to manage connections and requests to the RIT Client
     with requests.Session() as s:
@@ -107,10 +85,28 @@ def main():
                 s.post('http://localhost:9999/v1/commands/cancel?all=1')
                 sleep(1)
 
+            # delete orders older than 5 ticks
+            num_orders = len(orders)
+            while num_orders > 4:
+                orderid = orders[num_orders-1]['order_id']
+                s.delete('http://localhost:9999/v1/orders/{}'.format(orderid))
+                sleep(0.2)
+                orders = get_orders(s, 'OPEN')
+                num_orders = len(orders)
+
+            # delete orders when number of orders exceeds 4
+            num_orders = len(orders)
+            while num_orders > 4:
+                orderid = orders[num_orders-1]['order_id']
+                s.delete('http://localhost:9999/v1/orders/{}'.format(orderid))
+                sleep(0.2)
+                orders = get_orders(s, 'OPEN')
+                num_orders = len(orders)
+
+            sleep(1)
             # refresh the case time. THIS IS IMPORTANT FOR THE WHILE LOOP
             tick = get_tick(s)
 
-# this calls the main() method when you type 'python algo2.py' into the command prompt
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
     main()
